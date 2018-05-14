e2379301e87f42056daaebc76c4e11a51f895783
Pieter <pieter@lab.co.uk>
2018-05-14 22:04:59 +0200

    Headline Category

--- modules/custom/tonner/src/Commands/TonnerCommands.php
+++ modules/custom/tonner/src/Commands/TonnerCommands.php
@@ -205,7 +205,7 @@ class TonnerCommands extends DrushCommands {
                                 //Image
                                     //image file_name
                                     $ImageFileName = basename($article->urlToImage);
-                                    if(!empty($ImageFileName) && strlen($ImageFileName) < 255){
+                                    if(!empty($ImageFileName) && strlen($ImageFileName) < 200){
                                       //Talk
                                       echo '#######/ '.strlen($article->urlToImage).' / '.$article->urlToImage.' /################'.PHP_EOL;
                                       //Set the Image directory
