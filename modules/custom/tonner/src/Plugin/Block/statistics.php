7046894d84c57afa2cedc5adaa29d4248b07c3a5
Pieter <pieter@lab.co.uk>
2018-05-15 22:36:32 +0200

    News Sources

--- modules/custom/tonner/src/Plugin/Block/statistics.php
+++ modules/custom/tonner/src/Plugin/Block/statistics.php
@@ -36,7 +36,7 @@ class statistics extends BlockBase {
                 ->condition('field_tone',$tone['tid'],'=')
                 ->condition('field_publishedat',strtotime('-3 day'),'>=')
                 ->sort('field_publishedat')
-                ->range(0,10000)
+                ->range(0,1000)
                 ->execute();
             $nodes = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple($result);
             //Loop for historical
