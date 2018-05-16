9144c76ce0b39fcb19d957d1e1e45bc4476cce86
Pieter <pieter@lab.co.uk>
2018-05-16 16:48:29 +0200

    Search Form

--- modules/custom/tonner/src/Form/graphsearchform.php
+++ modules/custom/tonner/src/Form/graphsearchform.php
@@ -22,13 +22,27 @@ class graphsearchform extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    //Container
+    $options = [];
+    //Get the List of Countries
+    $query = \Drupal::entityQuery('taxonomy_term');
+    $query->condition('vid', "country");
+    $tids = $query->execute();
+    $terms = \Drupal\taxonomy\Entity\Term::loadMultiple($tids);
+    //Loop Countries
+    foreach($terms as $country) {
+      $countryTerm = \Drupal\taxonomy\Entity\Term::load($country->Id());
+      $options[$country->Id()] = $countryTerm->getName();
+    }
+    //Add to select list
     $form['countries'] = [
       '#type' => 'select',
       '#title' => $this->t('Countries'),
       '#description' => $this->t('Select either Country'),
-      '#value' => ['gb'=>'United Kingdom','us'=>'United States'],
+      '#options' => $options,
       '#weight' => '1',
     ];
+
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
