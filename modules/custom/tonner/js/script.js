86b6384ba86917eddb11f5dbaf97b78e4dd30c19
Pieter <pieter@lab.co.uk>
2018-05-18 16:28:10 +0200

    Watson Creds

--- modules/custom/tonner/js/script.js
+++ modules/custom/tonner/js/script.js
@@ -13,44 +13,44 @@
       //Current daily tones
       google.charts.setOnLoadCallback(currentPieChart);
       //Total Stats for today
-      function currentPieChart() {
-        //Set the Data holder and top line
-        var chartData = [
-          ['Tones', 'Headlines per Tone']
-        ];
-        //inject count into array
-        $.each(drupalSettings.tonescollect, function( index, value,data ) {
-          chartData.push([index,value.current_headline_count]);
-        });
-        var data = google.visualization.arrayToDataTable(chartData);
-        //set the title
-        var options = {
-          title: 'Tones Percentages for today, totaling: '+drupalSettings.current_total_headlines+' News Headlines',
-          tooltip: { trigger: 'selection' }
-        };
-        //draw chart on element
-        var currentlpiechart = new google.visualization.PieChart(document.getElementById('currentlpiechart'));
-
-        currentlpiechart.setAction({
-          id: 'sample',
-          text: 'See responsible Head Lines',
-          action: function() {
-            selection = currentlpiechart.getSelection();
-            switch (selection[0].row) {
-              case 0: alert('Ender\'s Game-'+ selection[0].row); break;
-              case 1: alert('Feynman Lectures on Physics-'+ selection[0].row); break;
-              case 2: alert('Numerical Recipes in JavaScript-'+ selection[0].row); break;
-              case 3: alert('Truman-'+ selection[0].row); break;
-              case 4: alert('Freakonomics-'+ selection[0].row); break;
-              case 5: alert('The Mezzanine-'+ selection[0].row); break;
-              case 6: alert('The Color of Magic-'+ selection[0].row); break;
-              case 7: alert('The Law of Superheroes-'+ selection[0].row); break;
-            }
-          }
-        });
-        //finalise
-        currentlpiechart.draw(data, options);
-      }
+      // function currentPieChart() {
+      //   //Set the Data holder and top line
+      //   var chartData = [
+      //     ['Tones', 'Headlines per Tone']
+      //   ];
+      //   //inject count into array
+      //   $.each(drupalSettings.tonescollect, function( index, value,data ) {
+      //     chartData.push([index,value.current_headline_count]);
+      //   });
+      //   var data = google.visualization.arrayToDataTable(chartData);
+      //   //set the title
+      //   var options = {
+      //     title: 'Tones Percentages for today, totaling: '+drupalSettings.current_total_headlines+' News Headlines',
+      //     tooltip: { trigger: 'selection' }
+      //   };
+      //   //draw chart on element
+      //   var currentlpiechart = new google.visualization.PieChart(document.getElementById('currentlpiechart'));
+      //
+      //   currentlpiechart.setAction({
+      //     id: 'sample',
+      //     text: 'See responsible Head Lines',
+      //     action: function() {
+      //       selection = currentlpiechart.getSelection();
+      //       switch (selection[0].row) {
+      //         case 0: alert('Ender\'s Game-'+ selection[0].row); break;
+      //         case 1: alert('Feynman Lectures on Physics-'+ selection[0].row); break;
+      //         case 2: alert('Numerical Recipes in JavaScript-'+ selection[0].row); break;
+      //         case 3: alert('Truman-'+ selection[0].row); break;
+      //         case 4: alert('Freakonomics-'+ selection[0].row); break;
+      //         case 5: alert('The Mezzanine-'+ selection[0].row); break;
+      //         case 6: alert('The Color of Magic-'+ selection[0].row); break;
+      //         case 7: alert('The Law of Superheroes-'+ selection[0].row); break;
+      //       }
+      //     }
+      //   });
+      //   //finalise
+      //   currentlpiechart.draw(data, options);
+      // }
       //Historical Graph
       google.charts.setOnLoadCallback(historicalChart);
       //Historical Chart
@@ -91,7 +91,7 @@
       }
       //Draw pie chart
       google.charts.setOnLoadCallback(totalPieChart);
-      google.charts.setOnLoadCallback(drawVisualization);
+      // google.charts.setOnLoadCallback(drawVisualization);
       //Pie Chat Total Function
       function totalPieChart() {
         //Set the Data holder and top line
@@ -105,46 +105,49 @@
         var data = google.visualization.arrayToDataTable(chartData);
         //set the title
         var options = {
-          title: 'Tones Percentages for all '+drupalSettings.total_headlines+' News Headlines over the last 5 days.'
+          title: 'Tones Percentages for all '+drupalSettings.total_headlines+' News Headlines.'
         };
+        if(typeof drupalSettings.graphheadline !== "undefined"){
+          options.title =  drupalSettings.graphheadline+' performance over '+drupalSettings.total_headlines+' headlines.';
+        }
         //draw chart on element
         var chart = new google.visualization.PieChart(document.getElementById('totalpiechart'));
         //finalise
         chart.draw(data, options);
       }
       //Curve chart
-      function drawVisualization() {
-        //set chart data
-        var chartData = [];
-        //inject headers
-        var headers = ['Date']
-        $.each(drupalSettings.tonescollect, function( index, value ) {
-          headers.push(index);
-        });
-        //push into first line of chart
-        chartData.push(headers);
-        //Push in data
-        $.each(drupalSettings.historialdata, function( index, value ) {
-          var ar = [index];
-          $.each(value, function(i,v){
-            ar.push(v);
-          });
-          chartData.push(ar);
-        });
-        //push into chart data
-        var data = google.visualization.arrayToDataTable(chartData);
-
-        var options = {
-          title : 'Tracking Anger across '+drupalSettings.total_headlines+' headlines over the last 5 days.',
-          vAxis: {title: 'Tone Intensity'},
-          hAxis: {title: 'date'},
-          seriesType: 'bars',
-          series: {1: {type: 'line'}}
-        };
-
-        var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
+      // function drawVisualization() {
+      //   //set chart data
+      //   var chartData = [];
+      //   //inject headers
+      //   var headers = ['Date']
+      //   $.each(drupalSettings.tonescollect, function( index, value ) {
+      //     headers.push(index);
+      //   });
+      //   //push into first line of chart
+      //   chartData.push(headers);
+      //   //Push in data
+      //   $.each(drupalSettings.historialdata, function( index, value ) {
+      //     var ar = [index];
+      //     $.each(value, function(i,v){
+      //       ar.push(v);
+      //     });
+      //     chartData.push(ar);
+      //   });
+      //   //push into chart data
+      //   var data = google.visualization.arrayToDataTable(chartData);
+      //
+      //   var options = {
+      //     title : 'Tracking Anger across '+drupalSettings.total_headlines+' headlines over the last 5 days.',
+      //     vAxis: {title: 'Tone Intensity'},
+      //     hAxis: {title: 'date'},
+      //     seriesType: 'bars',
+      //     series: {1: {type: 'line'}}
+      //   };
+      //
+      //   var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+      //   chart.draw(data, options);
+      // }
       /**
        * Styling on Forms
        */
