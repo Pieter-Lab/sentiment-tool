69bc1aa00fe4a507c935269930698f46cdd14f9f
Pieter <pieter@lab.co.uk>
2018-05-18 17:49:37 +0200

    Watson Creds

--- modules/custom/tonner/src/Plugin/Block/statistics.php
+++ modules/custom/tonner/src/Plugin/Block/statistics.php
@@ -142,7 +142,7 @@ class statistics extends BlockBase {
                 ]
             ],
             '#cache' => array('max-age' => 0),
-            '#topicsCloud'=> implode(', ',$tagsTopics)
+            '#topicsCloud'=> substr(implode(', ',$tagsTopics), 0, 250)
         ];
         if(isset($this->searchSentiment) && !empty($this->searchSentiment)){
           $build['#attached']['drupalSettings']['searchSentiment'] = ucfirst($this->searchSentiment);
