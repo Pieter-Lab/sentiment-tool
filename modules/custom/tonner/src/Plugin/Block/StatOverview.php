718c369380036909e0ee9060d1a36f3a68456ecd
Pieter Botha <pieter@lab.co.uk>
2018-06-22 14:19:11 +0200

    New Stats BLock

--- modules/custom/tonner/src/Plugin/Block/StatOverview.php
+++ modules/custom/tonner/src/Plugin/Block/StatOverview.php
@@ -18,10 +18,38 @@ class StatOverview extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    //Open build holder
     $build = [];
+    //Get All the Terms in the Countries Vocab
+    $query = \Drupal::entityQuery('taxonomy_term');
+    $query->condition('vid', "country");
+    $tids = $query->execute();
+    $terms = \Drupal\taxonomy\Entity\Term::loadMultiple($tids);
+    //Loop Countries
+    foreach($terms as $country) {
+      //Load country object
+      $countryTerm = \Drupal\taxonomy\Entity\Term::load($country->Id());
+      //get the sentiment Totals
+      $this->printer($countryTerm->field_sentiment_totals->getValue());
+    }
     $build['stats_overview']['#markup'] = 'Implement StatOverview.';
 
     return $build;
+  }
+
+  /**
+   * Prints out variables
+   */
+  public function printer($val){
+    if(is_array($val) || is_object($val)){
+      echo '<pre>';
+      print_r($val);
+      echo '</pre>';
+    }else{
+      echo '<br />';
+      var_dump($val);
+      echo '<br />';
+    }
   }
 
 }
